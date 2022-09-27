b944174a9debaf05ba1f64c192bb46b95a902490
Jose Rivasm <jirivasmo@gmail.com>
2022-09-27 09:28:09 -0500

    commented out s3 backend, removed sample lambda

--- Complete/main.tf
+++ Complete/main.tf
@@ -2,13 +2,14 @@ terraform {
   #This Backend has to be done after the s3 has been created
   #This will be applyed after a Terraform init -reconfigure to save
   #the state file on the bucket
-  backend "s3"{
-    #Cant use the Var.name since they have not yet been created
-    bucket  = "tfstate-s3-bucket-jirivasm"
-    #Path
-    key = "tfState/terraform.tfstate"
-    region = "us-east-1" 
-  }
+  
+  # backend "s3"{
+  #   #Cant use the Var.name since they have not yet been created
+  #   bucket  = "tfstate-s3-bucket-jirivasm"
+  #   #Path
+  #   key = "tfState/terraform.tfstate"
+  #   region = "us-east-1" 
+  # }
 }
 provider "aws"{
     region = var.region
